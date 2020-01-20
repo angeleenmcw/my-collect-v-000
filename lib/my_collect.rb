ace0b509a65307bd69e0ff9973f2a5cbf5b0818d
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-20 06:14:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,5 @@
-collection = ['ruby', 'javascript', 'python','objective-c']
+collection = ["ruby", "javascript", "python","objective-c"]
 def my_collect(collection) 
   collection(array) do |lang|
   lang.upcase
-end
 end
